9b75a91cc5e546093965dfb601e4e0ec10f0128c
Andrii Kruk <119102553+chodgi71@users.noreply.github.com>
2023-11-27 19:26:21 +0100

    create store, create auth-operations, selectors, slice, connect redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./pages/redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,5 +1,16 @@
+import { useDispatch } from "react-redux";
+import { signIn } from "../redux/auth/authOperations.js";
+
 const MainPage = () => {
-  return <section>Content on main page.</section>;
+  const dispatch = useDispatch();
+
+  const user = { email: "drop3r17@gmail.com", password: "123456789" };
+  return (
+    <section>
+      <p>Content on main page.</p>
+      <button onClick={() => dispatch(signIn(user))}>Click to login!</button>
+    </section>
+  );
 };
 
 export default MainPage;
