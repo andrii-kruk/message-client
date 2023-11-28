0aa50648b97f8f2280a6620b645065663674fb08
Andrii Kruk <119102553+chodgi71@users.noreply.github.com>
2023-11-28 07:42:15 +0100

    fix imports

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
-import "./index.css";
 import { Provider } from "react-redux";
-import { store } from "./pages/redux/store.js";
+import { store } from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,16 +1,5 @@
-import { useDispatch } from "react-redux";
-import { signIn } from "../redux/auth/authOperations.js";
-
 const MainPage = () => {
-  const dispatch = useDispatch();
-
-  const user = { email: "drop3r17@gmail.com", password: "123456789" };
-  return (
-    <section>
-      <p>Content on main page.</p>
-      <button onClick={() => dispatch(signIn(user))}>Click to login!</button>
-    </section>
-  );
+  return <section></section>;
 };
 
 export default MainPage;
