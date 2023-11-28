8ff1d12848521a61a7f527bd4e3593036975e5cc
Andrii Kruk <119102553+chodgi71@users.noreply.github.com>
2023-11-28 10:21:43 +0100

    add basename to BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import './index.css'
+import "./index.css";
 
 import App from "./App.jsx";
 import { Provider } from "react-redux";
@@ -10,7 +10,7 @@ import { store } from "./redux/store.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/message-client">
         <App />
       </BrowserRouter>
     </Provider>
--- vite.config.js
+++ vite.config.js
@@ -12,4 +12,5 @@ export default defineConfig({
       "@redux": path.resolve(__dirname, "src/redux"),
     },
   },
+  base: '/message-client/',
 });
